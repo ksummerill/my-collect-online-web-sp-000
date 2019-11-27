9fed35757d6c4391a4bc1e91614732854f2929f3
Kate Summerill <kaitlynsummerill@gmail.com>
2019-11-27 01:13:41 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,12 @@
+require 'pry'
+
 def my_collect(array)
   if block_given?
     collection = []
     i = 0
 
     while i < array.length
+
       collection << yield(array[i])
       i += 1
     end
